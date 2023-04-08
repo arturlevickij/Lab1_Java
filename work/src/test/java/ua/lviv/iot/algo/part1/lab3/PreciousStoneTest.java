42257cf078bc04707788e6ada03c33b762dfa695
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-04-08 17:59:07 +0300

    Create PreciousStoneTest.java

---
+++ work/src/test/java/ua/lviv/iot/algo/part1/lab3/PreciousStoneTest.java
@@ -0,0 +1,58 @@
+package ua.lviv.iot.algo.part1.lab3;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PreciousStoneTest {
+    private final PreciousStone diamond = new PreciousStone("Diamond", "Colorless", 1.5, 7.5, 10000, 3.0);
+    private final PreciousStone diamond2 = new PreciousStone("Diamond", "Colorless", 1.5, 7.5, 10000, 3.0);
+    private final PreciousStone diamond3 = new PreciousStone();
+
+    @Test
+    public void testGetTotalPrice() {
+        assertEquals(15000, diamond.getTotalPrice(), 0.01);
+    }
+
+    @Test
+    public void testIncreaseClarity() {
+        assertEquals(8.5, diamond.increaseClarity(), 0.01);
+    }
+
+    @Test
+    public void testIncreasePrice() {
+        assertEquals(15500, diamond.increasePrice(5), 0.01);
+    }
+
+    @Test
+    public void testGetFullPrice() {
+        assertEquals(15000, diamond.getFullPrice(), 0.01);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals("Diamond", diamond.getName());
+        assertEquals("Colorless", diamond.getColor());
+        assertEquals(1.5, diamond.getCarat(), 0.01);
+        assertEquals(7.5, diamond.getClarity(), 0.01);
+        assertEquals(10000, diamond.getPricePerCarat(), 0.01);
+        assertEquals(3.0, diamond.getWeightInGramm(), 0.01);
+    }
+
+    @Test
+    public void testToString() {
+        String expected = "PreciousStone{name='Diamond', color='Colorless', carat=1.5, clarity=7.5, pricePerCarat=10000.0, weightInGramm=3.0}";
+        assertEquals(expected, diamond.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        assertEquals(diamond, diamond2);
+    }
+
+    @Test
+    public void testNotEquals() {
+        PreciousStone emerald = new PreciousStone("Emerald", "Green", 1.5, 7.5, 10000, 3.0);
+        assertEquals(false, diamond.equals(emerald));
+    }
+}
