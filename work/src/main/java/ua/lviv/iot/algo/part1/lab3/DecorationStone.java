c52f34f09d8d53b93ea0f2dd9a9a60a6565500f0
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-04-08 17:54:29 +0300

    Create DecorationStone.java

---
+++ work/src/main/java/ua/lviv/iot/algo/part1/lab3/DecorationStone.java
@@ -0,0 +1,29 @@
+package ua.lviv.iot.algo.part1.lab3;
+
+import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+
+public class DecorationStone extends Stone{
+	private String type; 
+	private double priceForGramm;
+	private double weightInGramm;
+	
+	double getFullPrice() {
+		return(priceForGramm*weightInGramm);
+	}
+	
+	public DecorationStone(String name, String color, String type, double
+			  priceForGramm, double weightInGramm) { super(name, color); 
+			  this.type=type;
+			  this.priceForGramm=priceForGramm; 
+			  this.weightInGramm=weightInGramm; 
+			  }
+}
