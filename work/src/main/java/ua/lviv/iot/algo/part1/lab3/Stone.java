265573f3f78e7a7a95f97366f19b4c530c2afe71
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-04-08 17:51:50 +0300

    Create Stone.java

---
+++ work/src/main/java/ua/lviv/iot/algo/part1/lab3/Stone.java
@@ -0,0 +1,19 @@
+package ua.lviv.iot.algo.part1.lab3;
+
+import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+abstract class Stone {
+	protected String name;
+	protected String color;
+
+	abstract double getFullPrice();
+}
