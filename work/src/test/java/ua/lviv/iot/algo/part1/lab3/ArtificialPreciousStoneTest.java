c0c0f8727d42c1f9ef3052e409790222b3cefe2a
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-06-19 14:28:47 +0300

    Update ArtificialPreciousStoneTest.java

--- work/src/test/java/ua/lviv/iot/algo/part1/lab3/ArtificialPreciousStoneTest.java
+++ work/src/test/java/ua/lviv/iot/algo/part1/lab3/ArtificialPreciousStoneTest.java
@@ -5,20 +5,22 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class ArtificialPreciousStoneTest {
-    private final ArtificialPreciousStone diamond = new ArtificialPreciousStone("Diamond", "Clear", "Synthetic Diamonds", 5.5, 2.0);
-    private final ArtificialPreciousStone diamond2 = new ArtificialPreciousStone("Diamond", "Clear", "Synthetic Diamonds", 7.5, 3.0);
-    private final ArtificialPreciousStone diamond3 = new ArtificialPreciousStone();
-
+    private final ArtificialPreciousStone diamond = new ArtificialPreciousStone( "Diamond", "Blue",  "Lab1", 29.2, 36);
+    private final ArtificialPreciousStone diamond2 = new ArtificialPreciousStone( "Diamond", "Blue",  "Lab1", 29.2, 3.0);
     @Test
     public void testGetFullPrice() {
-        assertEquals(11.0, diamond.getFullPrice(), 0.0);
+        assertEquals(1051.2, diamond.getFullPrice(), 0.0);
     }
-
     @Test
     public void testSetWeightInGramm() {
         diamond.setWeightInGramm(3.0);
         assertEquals(diamond2.toString(), diamond.toString());
     }
+    @Test
+    public void testLabName() {
+        assertEquals("Lab1", diamond.getLabName());
+    }
+}
 
     @Test
     public void testLabName() {
